164149fc639c3503a26e8f9b65ca5416f908cf8b
xjtucoder <1055087125@qq.com>
2017-07-07 22:48:31 +0800

    add two

---
+++ DynamicProgram/best-time-to-buy-i.cpp
@@ -0,0 +1,16 @@
+// leetcode
+// time O(n) space O(1)
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+
+        int valley=prices[0];
+        int result=0;
+        for(int i=1;i<prices.size();++i)
+        {
+            result=max(result,prices[i]-valley);
+            valley=min(valley,prices[i]);
+        }
+        return result;
+    }
+};
